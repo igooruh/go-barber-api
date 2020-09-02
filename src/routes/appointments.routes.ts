9e7a6ead45b7a190ba1fdc2fe75a0ba8cdc37d9c
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-09-02 00:22:25 -0300

    Creating Repository: Refactoring router

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,26 +1,24 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
 
-import Appointment from '../models/appointment/Appointment';
+import AppointmentRepository from '../repositories/AppointmentRepository';
 
 const appointmentsRouter = Router();
 
-const appointments: Appointment[] = [];
+const appointmentRepository = new AppointmentRepository();
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
     const parsedDate = startOfHour(parseISO(date));
 
-    const findAppointmentInSameDate = appointments.find(appointment => isEqual(parsedDate, appointment.date));
+    const findAppointmentInSameDate = appointmentRepository.findByDate(parsedDate);
 
     if(findAppointmentInSameDate) {
         return response.status(400).json({  message: 'This appoiment is already booker' });
     }
 
-    const appointment = new Appointment(provider, parsedDate);
-
-    appointments.push(appointment);
+    const appointment = appointmentRepository.create(provider, parsedDate);
 
     return response.json(appointment);
 });
