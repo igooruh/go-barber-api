877e8871463659cce47c08f20a12b4de9338b7c7
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-09-02 01:47:37 -0300

    Response json list appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -4,8 +4,13 @@ import { startOfHour, parseISO } from 'date-fns';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 
 const appointmentsRouter = Router();
-
 const appointmentRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+    const appointments = appointmentRepository.appointmentAll();
+
+    return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
