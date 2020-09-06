4e7235e3067062f3c1b32393d20d87c1d183e4a2
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-09-06 11:53:33 -0300

    Creates relationship of tables appointments with users

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -3,8 +3,12 @@ import {
     Column,
     PrimaryGeneratedColumn,
     CreateDateColumn,
-    UpdateDateColumn
+    UpdateDateColumn,
+    ManyToOne,
+    JoinColumn
 } from 'typeorm';
+
+import User from './UserModel';
 
 @Entity('appointments')
 class Appointment {
@@ -12,7 +16,11 @@ class Appointment {
     id: string;
 
     @Column()
-    provider: string;
+    provider_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'provider_id' })
+    provider: User;
 
     @Column('time with time zone')
     date: Date;
