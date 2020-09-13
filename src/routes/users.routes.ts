661d1ed1019b784b0eecff8e164bf1863bd18b32
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-09-13 01:06:58 -0300

    Execute update avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import CreateUserService from '../services/CreateUsersService';
+import UpdateUserService from '../services/UpdateUserAvatarService';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import userDTO from '../mappers/UserMapper';
 import uploadConfig from '../config/upload';
@@ -29,8 +31,24 @@ usersRouter.post('/', async(request, response) => {
     }
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async(request, response) => {
-    return response.json({ ok: true });
+usersRouter.patch('/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async(request, response) => {
+
+    try {
+        const updateUserAvatar = new UpdateUserService();
+        const updateUser = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file.filename
+        });
+
+        const user = userDTO.toDTO(updateUser);
+
+        return response.json(user);
+    } catch(err) {
+        return response.status(400).json({ error: err.message });
+    }
 });
 
 export default usersRouter;
