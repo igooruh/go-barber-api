58103a4e2dcfa05857941766f0fcef9118722e65
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-09-13 01:00:37 -0300

    Serving static files

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+
+import uploadConfig from './config/upload';
 import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 export default app;
