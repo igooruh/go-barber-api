0508b48bacc0af45ac8099b1460865015d84ccb7
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-09-13 01:07:53 -0300

    Creates business rule for update avatar

---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,49 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import User from '../models/UserModel';
+import uploadConfig from '../config/upload';
+
+interface RequestUpdateUserAvatar {
+    user_id: string;
+    avatarFilename: string;
+}
+
+
+class UpdateUserAvatarService {
+    public async execute({ user_id, avatarFilename }: RequestUpdateUserAvatar): Promise<User> {
+        const userRepository = getRepository(User);
+
+        const user = await userRepository.findOne(user_id);
+
+        if(!user) {
+            throw new Error('Only authenticated users can change avatar');
+        }
+
+        validateFile(user);
+
+        user.avatar = avatarFilename;
+
+        await userRepository.save(user);
+
+        return user;
+    }
+}
+
+const validateFile = async({ avatar }: User) => {
+    if(avatar) {
+        const userAvatarFilaPath = path.join(uploadConfig.directory, avatar);
+        const userAvatarFileExists = await fs.promises.stat(userAvatarFilaPath);
+
+        fileExists(userAvatarFileExists, userAvatarFilaPath);
+    }
+}
+
+const fileExists = async(file: fs.Stats, userAvatarFilaPath: string) => {
+    if(file) {
+        await fs.promises.unlink(userAvatarFilaPath);
+    }
+}
+
+export default UpdateUserAvatarService;
