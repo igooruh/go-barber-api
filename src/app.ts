0f9dd4e44fa3a421f6c1016082d45f7fe613a009
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-10-12 21:45:08 -0300

    Add dependencies cors and types cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
@@ -7,6 +8,7 @@ import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
